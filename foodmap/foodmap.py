205a8723a22452f023297f2dce42fc1502b6ffaa
tilley14 <nmt2400@g.rit.edu>
2017-11-25 22:00:09 -0500

    Demonstration of using google maps
    Google Maps working on flask if provided valid api key

--- foodmap/foodmap.py
+++ foodmap/foodmap.py
@@ -10,9 +10,13 @@ app = Flask(__name__)
 def home():
     return "This is the home route. Just testing flask. By Kevin"
 
-@app.route('/<name>', methods=['GET'])
+@app.route('/vuetest/<name>', methods=['GET'])
 def hello_world(name=None):
     return render_template('hello.html', name=name)
+
+@app.route('/map', methods=['GET'])
+def test_map():
+    return render_template('maptest.html')
 
 if __name__ == '__main__':
     app.run()
