bc7403e858276b8bb99c2a73a233f032d8d4ea22
tilley14 <nmt2400@g.rit.edu>
2017-12-03 17:49:49 -0500

    Added key to maps page
    Key should be destroyed after testing and a new one made

--- foodmap/foodmap.py
+++ foodmap/foodmap.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 
 import logging
+import requests
 
 app = Flask(__name__)
 
@@ -10,14 +11,22 @@ app = Flask(__name__)
 def home():
     return "This is the home route. Just testing flask. By Kevin"
 
+
 @app.route('/vue/<name>', methods=['GET'])
 def hello_world(name=None):
     return render_template('hello.html', name=name)
+
 
 @app.route('/map', methods=['GET'])
 def test_map():
     return render_template('maptest.html')
 
+
+@app.route('/submit', methods=['POST'])
+def writeData(request):
+    #request.data
+    #write(data)
+    pass
 
 if __name__ == '__main__':
     app.run()
